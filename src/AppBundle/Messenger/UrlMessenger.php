a4e451e8ae23df0513de6a6d1d5b9aa4b90a1356
jeandiangelis <jean.angelis@gmail.com>
2016-06-11 10:46:31 -0300

    UrlMessnger now implements wamp

--- src/AppBundle/Messenger/UrlMessenger.php
+++ src/AppBundle/Messenger/UrlMessenger.php
@@ -3,49 +3,49 @@
 namespace AppBundle\Messenger;
 
 use Ratchet\ConnectionInterface;
-use Ratchet\MessageComponentInterface;
+use Ratchet\Wamp\Topic;
+use Ratchet\Wamp\WampServerInterface;
 
 /**
  * Class UrlMessenger
  */
-class UrlMessenger implements MessageComponentInterface
+class UrlMessenger implements WampServerInterface
 {
-    protected $clients;
-
-    public function __construct() {
-        $this->clients = new \SplObjectStorage;
-    }
-
     public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
-        $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";
-    }
-
-    public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
-        }
     }
 
     public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
+    public function onCall(ConnectionInterface $conn, $id, $topic, array $params) {
+        // In this application if clients send data it's because the user hacked around in console
+        $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
+    }
 
+    public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible) {
+        // In this application if clients send data it's because the user hacked around in console
         $conn->close();
+    }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+
+    /**
+     * A request to subscribe to a topic has been made
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic to subscribe to
+     */
+    function onSubscribe(ConnectionInterface $conn, $topic)
+    {
+        // TODO: Implement onSubscribe() method.
+    }
+
+    /**
+     * A request to unsubscribe from a topic has been made
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic to unsubscribe from
+     */
+    function onUnSubscribe(ConnectionInterface $conn, $topic)
+    {
+        // TODO: Implement onUnSubscribe() method.
     }
 }
